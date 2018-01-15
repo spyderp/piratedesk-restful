fda581234114872677764e4bb52c2e7d83ba9de4
Ricardo Portillo <ricardo.portillo@meduca.gob.pa>
2018-01-15 15:59:21 -0500

    cambios

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,11 @@
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+# from common.association_table import association
+
 
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+	id 					= db.Column(db.Integer, primary_key=True)
 	username 			= db.Column(db.String(30), index=True, unique=True)
 	password  			= db.Column(db.String(128))
 	nombre     		= db.Column(db.String(15))
@@ -13,11 +15,19 @@ class User(db.Model):
 	creado      		= db.Column(db.DateTime, default=datetime.utcnow)
 	modificado      	= db.Column(db.DateTime)
 	ultimo_acceso      = db.Column(db.DateTime)
-	puntaje 	= db.Column(db.Integer)
+	puntaje 			= db.Column(db.Integer)
+	rol_id 				= db.Column(db.Integer, db.ForeignKey('rol.id'))
+	# assignments 		= db.relationship('Assignment')
+	# histories 			= db.relationship('History')
+	# knowledges 		= db.relationship('Knowledge')
+	# messages 			= db.relationship('Message')
+	# tickets 				= db.relationship('Ticket')
+	# deparments		 = db.relationship('Deparment', secondary=association.deparments , lazy='subquery', backref=db.backref('deparments', lazy=True))
+	
 	def __repr__(self):
 		return '<User %r %r>' % (self.nombre, self.apellido)
 	def set_password(self, password):
-        self.password = generate_password_hash(password)
+		self.password = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+	def check_password(self, password):
+		return check_password_hash(self.password, password)
--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-	SQLALCHEMY_DATABASE_URI ='mysql://root:@localhost/piratedesk'
+	SQLALCHEMY_DATABASE_URI ='mysql://prueba:prueba@localhost/piratedesk2'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
